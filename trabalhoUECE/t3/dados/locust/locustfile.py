f65aed54eae32c57aef0192cbef0b754a037627b
Danilson Macedo <danilson50@gmail.com>
2020-01-27 17:28:32 +0000

    Atualização do Locust

--- trabalhoUECE/t3/dados/locust/locustfile.py
+++ trabalhoUECE/t3/dados/locust/locustfile.py
@@ -2,7 +2,11 @@ from locust import HttpLocust, TaskSet, task
 
 class UserTasks(TaskSet):
 
-    @task
+    @task(2)
+    def language(self):
+        self.client.post("/wp-admin/install.php?step1",{"language":""})
+
+    @task(1)
     def index(self):
         self.client.get("/?p=1") # Realiza um get na url <HOST_DO_WORDPRESS>/?p=1
 
