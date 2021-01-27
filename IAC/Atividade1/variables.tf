226a926fc5b9de9208e0b1c25d3be98960287139
Danilson Macedo <danilson50@gmail.com>
2021-01-27 16:05:23 -0300

    Atualização de arquivos 2

--- IAC/Atividade1/variables.tf
+++ IAC/Atividade1/variables.tf
@@ -3,27 +3,27 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-  default = "192.168.0.0/16"
+  default = "172.20.0.0/16"
 }
 
 variable "public_a_cidr" {
-  default = "192.168.1.0/24"
+  default = "172.20.1.0/24"
 }
 
 variable "private_a_cidr" {
-  default = "192.168.2.0/23"
+  default = "172.20.2.0/23"
 }
 
 variable "public_b_cidr" {
-  default = "192.168.3.0/24"
+  default = "172.20.3.0/24"
 }
 
 variable "private_b_cidr" {
-  default = "192.168.4.0/23"
+  default = "172.20.4.0/23"
 }
 
 variable "ami" {
-  default = "ami-0080e4c5bc078760e"
+  default = "ami-005b753c07ecef59f"
 }
 
 variable "instance_type" {
